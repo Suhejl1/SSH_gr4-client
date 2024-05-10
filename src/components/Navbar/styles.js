4d685ff7c314be78c6d937bd8b6ff58fc53bd89e
Reis Stanovci <112658989+reis03@users.noreply.github.com>
2024-05-10 16:27:23 +0200

    Revert "Feature/e4 t3 suhejl"

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -13,50 +13,47 @@ export default makeStyles((theme) => ({
       marginLeft: drawerWidth,
     },
   },
-  toolbar: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    [theme.breakpoints.down("sm")]: {
-      padding: theme.spacing(0, 0.5),
-    },
-  },
   title: {
+    flexGrow: 1,
     alignItems: "center",
     display: "flex",
     fontFamily: "Raleway",
     fontWeight: 600,
     letterSpacing: 1,
     textDecoration: "none",
-    marginRight: theme.spacing(1),
+    "&:hover": {
+      color: "#ffff",
+      boxShadow: "none",
+    },
+  },
+  cartt: {
     "&:hover": {
       color: "#ffff",
       boxShadow: "none",
     },
   },
   image: {
-    marginRight: theme.spacing(1),
-    height: "30px",
+    marginRight: "10px",
   },
-  buttonContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: theme.spacing(1),
-  },
-  navbarButton: {
-    padding: theme.spacing(1), // Adjust the padding as needed
-    marginLeft: theme.spacing(1), // Adjust the margin as needed
-    "&:hover": {
-      backgroundColor: theme.palette.primary.main, // Change the background color on hover
-      color: theme.palette.primary.contrastText, // Change the text color on hover
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
     },
   },
-  logoutButton: {
-    padding: theme.spacing(1), // Ensure consistent padding for the IconButton
-    "& > .MuiIconButton-label": {
-      width: "auto", // Set width to auto to adjust button width according to its content
+  grow: {
+    flexGrow: 1,
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
