64af05d10ad593a8d88e9762e05520b460df907c
Suhejl Vejseli <suhejl.vejseli03@gmail.com>
2024-05-12 18:33:07 +0200

    feature/E4-T3: Return UI based on role

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -21,7 +21,7 @@ const SignUp = () => {
         // Implement registration logic here (e.g., send registration request to server)
 
         // Redirect to "/home" path on successful registration
-        history.push('/home');
+        history.push('/');
     };
 
     return (
